82d8ccaaa8a251651d5764bcca95a409e48061d2
bhagyalakshmiganipisetty <114427542+bhagyalakshmiganipisetty@users.noreply.github.com>
2024-06-07 21:20:31 +0530

    Add files via upload

---
+++ bhagi/src/main/java/qwerty/bhagi/App.java
@@ -0,0 +1,13 @@
+package qwerty.bhagi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+
+    }
+}
